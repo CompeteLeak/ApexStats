6ae9f682d8a67958a0beeadfb00d4710db6e0049
maw <malikiw20@gmail.com>
2023-12-02 21:17:56 -0500

    format rank score and fix sort method

--- src/main/java/ApexApiCall.java
+++ src/main/java/ApexApiCall.java
@@ -1,134 +1,123 @@
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.URI;
+import java.text.DecimalFormat;
 import java.time.Duration;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import java.text.DecimalFormat;
-import org.json.JSONObject;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ApexApiCall {
+    private static final int MAX_RETRIES = 3;
+    private static final int INITIAL_BACKOFF_DELAY = 1000; // Initial delay in milliseconds
 
-  private static final int MAX_RETRIES = 3;
-  private static final int INITIAL_BACKOFF_DELAY = 1000; // Initial delay in milliseconds
+    private long getBackoffTime(int retryCount) {
+        // Exponential backoff formula (adjust as needed)
+        return (long) (INITIAL_BACKOFF_DELAY * Math.pow(2, retryCount));
+    }
 
-  private long getBackoffTime(int retryCount) {
-    // Exponential backoff formula (adjust as needed)
-    return (long)(INITIAL_BACKOFF_DELAY * Math.pow(2, retryCount));
-  }
+    public void getPlayerDataByName(String apiKey, String playerName, String platform, int retryCount) {
+        String API_URL = "https://api.mozambiquehe.re/bridge?auth=%s&player=%s&platform=%s";
+        String apiUrl = String.format(API_URL, apiKey, playerName, platform);
+        makeApiRequest(apiUrl, playerName, retryCount + 1);
+    }
 
-  public void getPlayerDataByName(String apiKey, String playerName, String platform, int retryCount) {
-    String API_URL = "https://api.mozambiquehe.re/bridge?auth=%s&player=%s&platform=%s";
-    String apiUrl = String.format(API_URL, apiKey, playerName, platform);
-    makeApiRequest(apiUrl,playerName, retryCount +1);
-  }
+    private void makeApiRequest(String apiUrl, String playerName, int retryCount) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiUrl)).timeout(Duration.ofSeconds(10)).build();
 
-  // public void getPlayerDataByUID(String apiKey, String playerUID, String platform) {
-  //   String API_URL = "https://api.mozambiquehe.re/bridge?auth=%s&uid=%s&platform=%s";
-  //   String apiUrl = String.format(API_URL, apiKey, playerUID, platform);
-  //   //makeApiRequest(apiUrl);
-  // }
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(this::handleResponse)
+                .exceptionally(ex -> {
+                    if (ex.getMessage().contains("429") && retryCount < MAX_RETRIES) {
+                        // Apply exponential backoff before retrying
+                        try {
+                            Thread.sleep(getBackoffTime(retryCount));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        makeApiRequest(apiUrl, playerName, retryCount + 1);
+                    } else {
+                        handleError(ex);
+                    }
+                    return null;
+                })
+                .thenAccept(stats -> handleData(stats, playerName));
+    }
 
-  private void makeApiRequest(String apiUrl, String playerName, int retryCount) {
-    HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder()
-      .uri(URI.create(apiUrl))
-      .timeout(Duration.ofSeconds(10))
-      .build();
-
-    client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-      .thenApply(this::handleResponse)
-      .exceptionally(ex -> {
-        if (ex.getMessage().contains("429") && retryCount < MAX_RETRIES) {
-          // Apply exponential backoff before retrying
-          try {
-            Thread.sleep(getBackoffTime(retryCount));
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-          makeApiRequest(apiUrl, playerName, retryCount + 1);
+    private String handleResponse(HttpResponse<String> response) {
+        int statusCode = response.statusCode();
+        if (statusCode >= 200 && statusCode < 300) {
+            return response.body();
         } else {
-          handleError(ex);
+            throw new RuntimeException("API request failed with status code: " + statusCode);
         }
-        return null;
-      })
-      .thenAccept(stats -> handleData(stats, playerName));
-  }
-
-  private String handleResponse(HttpResponse < String > response) {
-    int statusCode = response.statusCode();
-    if (statusCode >= 200 && statusCode < 300) {
-      return response.body();
-    } else {
-      throw new RuntimeException("API request failed with status code: " + statusCode);
-    }
-  }
-
-  private String handleError(Throwable ex) { // Modified the return type to match the expected type
-    Platform.runLater(() -> {
-      Alert alert = new Alert(AlertType.ERROR);
-      alert.setTitle("Error");
-      alert.setHeaderText("API Request Failed");
-      alert.setContentText(ex.getMessage());
-      alert.showAndWait();
-    });
-    return ""; // Return an empty string to align with thenApply's expected return type
-  }
-
-  private void handleData(String stats, String playerName) {
-    DecimalFormat formatter = new DecimalFormat("#,###");
-
-    if (stats == null) {
-      // Error occurred, handle accordingly
-      return;
     }
 
-    try {
-      JSONObject json = new JSONObject(stats);
-
-      // Extracting data from the "global" section
-      JSONObject global = json.getJSONObject("global");
-      int rankScore = global.getJSONObject("rank").getInt("rankScore");
-      String rank = global.getJSONObject("rank").getString("rankName");
-      int rankDivision = global.getJSONObject("rank").getInt("rankDiv");
-
-      // Extracting data from the "legends" section for the selected legend (Vantage)
-      JSONObject legends = json.getJSONObject("legends");
-      JSONObject selectedLegend = legends.getJSONObject("selected");
-      String legendName = selectedLegend.getString("LegendName");
-      JSONArray legendData = selectedLegend.getJSONArray("data");
-      int brKills = 0;
-      int brDamage = 0;
-      int brWins = 0;
-      for (int i = 0; i < legendData.length(); i++) {
-        JSONObject stat = legendData.getJSONObject(i);
-        if (stat.getString("name").equals("BR Kills")) {
-          brKills = stat.getInt("value");
-        }
-        if (stat.getString("name").equals("BR Damage")) {
-          brDamage = stat.getInt("value");
-        }
-        if (stat.getString("name").equals("BR Wins")) {
-          brWins = stat.getInt("value");
-          //break;
-        }
-      }
-
-      // Update the UI with the retrieved information
-      ApexStatsApp.updateUI(playerName, rank, rankScore,rankDivision,legendName, formatter.format(brKills), formatter.format(brDamage), formatter.format(brWins));
-    } catch (Exception e) {
-      e.printStackTrace();
-      Platform.runLater(() -> {
-        Alert alert = new Alert(AlertType.ERROR);
-        alert.setTitle("Error");
-        alert.setHeaderText("Data Processing Failed");
-        alert.setContentText("An error occurred while processing the API response.");
-        alert.showAndWait();
-      });
+    private String handleError(Throwable ex) {
+        Platform.runLater(() -> {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("API Request Failed");
+            alert.setContentText(ex.getMessage());
+            alert.showAndWait();
+        });
+        return ""; // Return an empty string to align with thenApply's expected return type
     }
-  }
 
+    private void handleData(String stats, String playerName) {
+        DecimalFormat formatter = new DecimalFormat("#,###");
+
+        if (stats == null) {
+            // Error occurred, handle accordingly
+            return;
+        }
+
+        try {
+            JSONObject json = new JSONObject(stats);
+
+            // Extracting data from the "global" section
+            JSONObject global = json.getJSONObject("global");
+            int rankScore = global.getJSONObject("rank").getInt("rankScore");
+            String rank = global.getJSONObject("rank").getString("rankName");
+            int rankDivision = global.getJSONObject("rank").getInt("rankDiv");
+
+            // Extracting data from the "legends" section for the selected legend (Vantage)
+            JSONObject legends = json.getJSONObject("legends");
+            JSONObject selectedLegend = legends.getJSONObject("selected");
+            String legendName = selectedLegend.getString("LegendName");
+            JSONArray legendData = selectedLegend.getJSONArray("data");
+            int brKills = 0;
+            int brDamage = 0;
+            int brWins = 0;
+
+            for (int i = 0; i < legendData.length(); i++) {
+                JSONObject stat = legendData.getJSONObject(i);
+                if (stat.getString("name").equals("BR Kills")) {
+                    brKills = stat.getInt("value");
+                }
+                if (stat.getString("name").equals("BR Damage")) {
+                    brDamage = stat.getInt("value");
+                }
+                if (stat.getString("name").equals("BR Wins")) {
+                    brWins = stat.getInt("value");
+                }
+            }
+
+            // Update the UI with the retrieved information
+            ApexStatsApp.updateUI(playerName, rank, formatter.format(rankScore), rankDivision, legendName, formatter.format(brKills), formatter.format(brDamage), formatter.format(brWins));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Platform.runLater(() -> {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Data Processing Failed");
+                alert.setContentText("An error occurred while processing the API response.");
+                alert.showAndWait();
+            });
+        }
+    }
 }
--- src/main/java/ApexStatsApp.java
+++ src/main/java/ApexStatsApp.java
@@ -1,144 +1,154 @@
+import java.util.Comparator;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.geometry.Insets;
-import java.util.Comparator;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 public class ApexStatsApp extends Application {
+    private static TableView<Player> tableView;
+    private static ObservableList<Player> playerList;
 
-  private static TableView < Player > tableView;
-  private static ObservableList < Player > playerList;
-
-  public static void main(String[] args) {
-    launch(args);
-  }
-
-  @Override
-  public void start(Stage primaryStage) {
-    playerList = FXCollections.observableArrayList();
-    tableView = new TableView < > (playerList);
-
-    TableColumn < Player, String > playerColumn = new TableColumn < > ("Player");
-    playerColumn.setCellValueFactory(new PropertyValueFactory < > ("name"));
-
-    TableColumn < Player, String > rankColumn = new TableColumn < > ("Rank");
-    rankColumn.setCellValueFactory(new PropertyValueFactory < > ("rankName"));
-
-    TableColumn < Player, Integer > rankScoreColumn = new TableColumn < > ("Rank Score");
-    rankScoreColumn.setCellValueFactory(new PropertyValueFactory < > ("rankScore"));
-
-    TableColumn < Player, Integer > rankDivColumn = new TableColumn < > ("Rank Division");
-    rankDivColumn.setCellValueFactory(new PropertyValueFactory < > ("rankDiv"));
-
-    TableColumn < Player, String > legendCoulumn = new TableColumn < > ("Tracked Legend");
-    legendCoulumn.setCellValueFactory(new PropertyValueFactory < > ("legendName"));
-
-    TableColumn < Player, String > killsColumn = new TableColumn < > ("Kills");
-    killsColumn.setCellValueFactory(new PropertyValueFactory < > ("kills"));
-
-    TableColumn<Player, String> damageColumn = new TableColumn<>("Damage");
-    damageColumn.setCellValueFactory(new PropertyValueFactory<>("damage"));
-
-    TableColumn<Player, String> winsColumn = new TableColumn<>("Wins");
-    winsColumn.setCellValueFactory(new PropertyValueFactory<>("wins"));
-
-    tableView.getColumns().addAll(playerColumn, rankColumn, rankScoreColumn, rankDivColumn, legendCoulumn, killsColumn, damageColumn, winsColumn);
-    sortPlayerList(); // Sort the player list initially
-
-    VBox vbox = new VBox(tableView);
-    vbox.setPadding(new Insets(10));
-
-    Scene scene = new Scene(vbox, 400, 300);
-
-    primaryStage.setTitle("Leaderboards");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-
-    ApexApiCall apiCall = new ApexApiCall();
-    YamlVars envLoad = new YamlVars();
-    envLoad.connectYaml();
-
-    apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerOne, envLoad.platform1, 0);
-    apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerTwo, envLoad.platform1, 0);
-    apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerThree, envLoad.platform1, 0);
-    apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerFour, envLoad.platform1, 0);
-    apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerFive, envLoad.platform2, 0);
-    apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerSix, envLoad.platform1, 0);
-    apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerSeven, envLoad.platform1, 0);
-  }
-
-  public static void updateUI(String name, String rankName, int rankScore, int rankDiv, String legendName, String kills, String damage, String wins) {
-    Platform.runLater(() -> {
-      playerList.add(new Player(name, rankName, rankScore, rankDiv, legendName, kills, damage, wins));
-      sortPlayerList();
-    });
-  }
-
-  private static void sortPlayerList() {
-    playerList.sort(Comparator.comparingInt(Player::getRankScore).reversed());
-  }
-
-  public static class Player {
-    private final String name;
-    private final String rankName;
-    private final int rankScore;
-    private final int rankDiv;
-    public final String legendName;
-    private final String kills;
-    private final String damage;
-    private final String wins;
-
-    public Player(String name, String rankName, int rankScore, int rankDiv, String legendName, String kills, String damage, String wins) {
-      this.name = name;
-      this.rankName = rankName;
-      this.rankScore = rankScore;
-      this.rankDiv = rankDiv;
-      this.legendName = legendName;
-      this.kills = kills;
-      this.damage = damage;
-      this.wins = wins;
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public String getName() {
-      return name;
+    @Override
+    public void start(Stage primaryStage) {
+        playerList = FXCollections.observableArrayList();
+        tableView = new TableView<>(playerList);
+
+        TableColumn<Player, String> playerColumn = new TableColumn<>("Player");
+        playerColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Player, String> rankColumn = new TableColumn<>("Rank");
+        rankColumn.setCellValueFactory(new PropertyValueFactory<>("rankName"));
+
+        TableColumn<Player, String> rankScoreColumn = new TableColumn<>("Rank Score");
+        rankScoreColumn.setCellValueFactory(new PropertyValueFactory<>("rankScore"));
+
+        TableColumn<Player, Integer> rankDivColumn = new TableColumn<>("Rank Division");
+        rankDivColumn.setCellValueFactory(new PropertyValueFactory<>("rankDiv"));
+
+        TableColumn<Player, String> legendColumn = new TableColumn<>("Tracked Legend");
+        legendColumn.setCellValueFactory(new PropertyValueFactory<>("legendName"));
+
+        TableColumn<Player, String> killsColumn = new TableColumn<>("Kills");
+        killsColumn.setCellValueFactory(new PropertyValueFactory<>("kills"));
+
+        TableColumn<Player, String> damageColumn = new TableColumn<>("Damage");
+        damageColumn.setCellValueFactory(new PropertyValueFactory<>("damage"));
+
+        TableColumn<Player, String> winsColumn = new TableColumn<>("Wins");
+        winsColumn.setCellValueFactory(new PropertyValueFactory<>("wins"));
+
+        tableView.getColumns().addAll(playerColumn, rankColumn, rankScoreColumn, rankDivColumn, legendColumn, killsColumn, damageColumn, winsColumn);
+        sortPlayerList(); // Sort the player list initially
+
+        VBox vbox = new VBox(tableView);
+        vbox.setPadding(new Insets(10));
+
+        Scene scene = new Scene(vbox, 400, 300);
+
+        primaryStage.setTitle("Leaderboards");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        ApexApiCall apiCall = new ApexApiCall();
+        YamlVars envLoad = new YamlVars();
+        envLoad.connectYaml();
+
+        apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerOne, envLoad.platform1, 0);
+        apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerTwo, envLoad.platform1, 0);
+        apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerThree, envLoad.platform1, 0);
+        apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerFour, envLoad.platform1, 0);
+        apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerFive, envLoad.platform2, 0);
+        apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerSix, envLoad.platform1, 0);
+        apiCall.getPlayerDataByName(envLoad.apiKey, envLoad.playerSeven, envLoad.platform1, 0);
     }
 
-    public String getRankName() {
-      return rankName;
+    public static void updateUI(String name, String rankName, String rankScore, int rankDiv,
+                                String legendName, String kills, String damage, String wins) {
+        Platform.runLater(() -> {
+            playerList.add(new Player(name, rankName, rankScore, rankDiv, legendName, kills, damage, wins));
+            sortPlayerList();
+        });
     }
 
-    public int getRankScore() {
-      return rankScore;
+    private static void sortPlayerList() {
+        playerList.sort(Comparator
+                .comparingInt(player -> {
+                    try {
+                        return Integer.parseInt(((Player) player)
+                                .getRankScore()
+                                .replaceAll(",", "")
+                                .trim());
+                    } catch (NumberFormatException e) {
+                        return 0;
+                    }
+                })
+                .reversed());
     }
 
-    public int getRankDiv(){
-      return rankDiv;
+    public static class Player {
+        private final String name;
+        private final String rankName;
+        private final String rankScore;
+        private final int rankDiv;
+        public final String legendName;
+        private final String kills;
+        private final String damage;
+        private final String wins;
+
+        public Player(String name, String rankName, String rankScore, int rankDiv, String legendName, String kills, String damage, String wins) {
+            this.name = name;
+            this.rankName = rankName;
+            this.rankScore = rankScore;
+            this.rankDiv = rankDiv;
+            this.legendName = legendName;
+            this.kills = kills;
+            this.damage = damage;
+            this.wins = wins;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getRankName() {
+            return rankName;
+        }
+
+        public String getRankScore() {
+            return rankScore;
+        }
+
+        public int getRankDiv() {
+            return rankDiv;
+        }
+
+        public String getLegendName() {
+            return legendName;
+        }
+
+        public String getKills() {
+            return kills;
+        }
+
+        public String getDamage() {
+            return damage;
+        }
+
+        public String getWins() {
+            return wins;
+        }
     }
-
-    public String getLegendName(){
-      return legendName;
-    }
-
-    public String getKills() {
-      return kills;
-    }
-
-    public String getDamage() {
-      return damage;
-   }
-
-   public String getWins(){
-      return wins;
-   }
-  }
-
 }
